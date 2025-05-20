dd6d9ef507e9bc22a2719aed69c5e37260aeb943
GabrielKenzoT <gabrieltakeuchi418@gmail.com>
2025-05-20 14:40:26 -0300

    Fazendo appTrabCrudTotal_GabrielKenzo

---
+++ appTrabCrudTotal_GabrielKenzo/src/routes/stack.routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerRoutes from './drawer.routes';
+
+import Home from '../screens/Home';
+import Login from '../screens/Login'
+import Cadastro from '../screens/Cadastro';
+
+
+const Stack = createNativeStackNavigator();
+
+function StackNavigator(){
+    return (
+    
+        <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>   
+
+            <Stack.Screen name="Login" component={Login} />  
+            <Stack.Screen name="Home" component={DrawerRoutes} />                          
+            <Stack.Screen name="Cadastro" component={Cadastro} /> 
+
+        </Stack.Navigator>
+    )
+}
+
+function AppRoutes(){
+    return(
+        <NavigationContainer>
+        <DrawerRoutes/>
+      </NavigationContainer>
+    )
+}
+export default AppRoutes;
---
+++ appTrabCrudTotal_GabrielKenzo/src/screens/Login/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { ScrollView,Alert, Text, TextInput, View, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { styles } from './style';
+import { showMessage, hideMessage } from "react-native-flash-message";
+
+import api from '../../../services/api';
+
+export default function Login({navigation}){
+
+    const [email,setEmail] = useState('');
+    const [senha,setSenha] = useState('');
+    const [isSelected, setSelection] = useState(false);
+
+    return (
+        <View style={styles.container}>
+          <Text style={styles.logo}>Comidas e Mais Comidas</Text>
+          <Text style={styles.titulo}>Login</Text>
+      
+          <View style={styles.area}>
+            <Text style={styles.texto}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Entre com seu endereço de Email"
+              placeholderTextColor={"#000842"}
+              onChangeText={email => setEmail(email)}
+            />
+            <Text style={styles.texto}>Senha</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Digite sua senha"
+              placeholderTextColor={"#000842"}
+              secureTextEntry={true}
+              onChangeText={senha => setSenha(senha)}
+            />
+          </View>
+      
+          <TouchableOpacity
+            style={styles.botao}
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Text style={styles.textoBotao}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      );
+}
+
