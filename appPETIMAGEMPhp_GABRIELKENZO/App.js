2afc2bd87f25e6dd48758f292cca2f870104ee85
GabrielKenzoT <gabrieltakeuchi418@gmail.com>
2025-08-22 14:34:57 -0300

    Pets

--- appPETIMAGEMPhp_GABRIELKENZO/App.js
+++ appPETIMAGEMPhp_GABRIELKENZO/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {View,Text,ActivityIndicator,StyleSheet,Image,Dimensions,SafeAreaView,TouchableOpacity,ImageBackground} from 'react-native';
+import {View,Text,ActivityIndicator,StyleSheet,Image,Dimensions,SafeAreaView,TouchableOpacity,Alert, ScrollView} from 'react-native';
 
 import Carousel from 'react-native-reanimated-carousel';
 import * as ImagePicker from "expo-image-picker";
@@ -54,7 +54,7 @@ export default function App() {
       formData.append('photo', { uri: image, name: filename, type });
 
       try {
-        const response = await fetch("http://10.239.0.124/imagem/upload.php", {
+        const response = await fetch("http://10.239.0.125/imagem/upload.php", {
           method: 'POST',
           body: formData,
           headers: {
@@ -77,7 +77,7 @@ export default function App() {
   useEffect(() => {
     async function carregarImagens() {
       try {
-        const response = await fetch('http://10.239.0.124/imagem/listar_imagens.php');
+        const response = await fetch('http://10.239.0.125/imagem/lista_imagens.php');
         const data = await response.json();
         setImagens(data);
       } catch (error) {
@@ -100,8 +100,8 @@ export default function App() {
 
 
  return (
+  <ScrollView>
     <View style={styles.container}>
-      <ImageBackground source={require('../../assets/img/fundo2.jpg')} style={styles.imgBg} >
       <SafeAreaView style={styles.safeArea}>
         <Text style={styles.header}>Você viu esses pets?</Text>
         <Text style={styles.header}>Caso Viu contantar o número 66-666-666-666</Text>
@@ -135,17 +135,13 @@ export default function App() {
           <Text style={styles.buttonText}>Tirar Foto</Text>
         </TouchableOpacity>
 
-        {image && (
-          <Image source={{ uri: image }} style={styles.image} />
-        )}
-
         <TouchableOpacity style={styles.button} onPress={uploadImage}>
           <Ionicons name="cloud-upload" size={20} color="white" />
           <Text style={styles.buttonText}>Enviar foto</Text>
         </TouchableOpacity>    
       </SafeAreaView>
-          </ImageBackground>
     </View>
+    </ScrollView>
   );
 }
 
@@ -154,6 +150,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    height:1100,
     
   },
   button: {
